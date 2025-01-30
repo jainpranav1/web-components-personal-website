2a5d4b6438f20fa48aca05687b8ad9c9c02f4cb8
Pranav Jain <the.pranav.jain.1@gmail.com>
2025-01-29 20:56:49 -0600

    added navbar

---
+++ components/NavBar.js
@@ -0,0 +1,65 @@
+/*
+Helpful links:
+- https://www.w3schools.com/css/css_navbar_horizontal.asp
+*/
+
+class NavBar extends HTMLElement {
+  constructor() {
+    super();
+
+    const template = document.createElement("template");
+    template.innerHTML = /*html*/ `
+        <style>
+            ul {
+                list-style-type: none;
+                margin: 0;
+                padding: 0;
+                overflow: hidden;
+                background-color: #333;
+            }
+
+            li {
+                float: left;
+                border-right: 1px solid #bbb;
+            }
+
+            li a {
+                display: block;
+                color: white;
+                text-align: center;
+                padding: 14px 16px;
+                text-decoration: none; /* remove underline */
+            }
+
+            li a:hover {
+            background-color: #111;
+            }
+        </style>
+
+        <nav>
+            <ul>
+                <li>
+                    <a href="/">Home</a>
+                </li>
+                <li>
+                    <a href="/">My Story</a>
+                </li>
+                <li>
+                    <a href="/">Education</a>
+                </li>
+                <li>
+                    <a href="/">Jobs</a>
+                </li>
+                <li>
+                    <a href="/">Blog</a>
+                </li>
+            </ul>
+        </nav>
+    `;
+
+    const shadow = this.attachShadow({ mode: "open" });
+    shadow.append(template.content.cloneNode(true));
+  }
+}
+
+customElements.define("navbar-elem", NavBar);
--- server.js
+++ server.js
@@ -2,15 +2,26 @@ Deno.serve(async (req) => {
   if (req.method === "GET") {
     const url = new URL(req.url);
 
+    // pages
     if (url.pathname === "/") {
       return new Response(await Deno.readFile("pages/home.html"), {
         headers: { "content-type": "text/html" },
       });
-    } else if (url.pathname === "/components/Face.js") {
+    }
+
+    // components
+    else if (url.pathname === "/components/Face.js") {
       return new Response(await Deno.readFile("components/Face.js"), {
         headers: { "content-type": "text/javascript" },
       });
-    } else if (url.pathname === "/images/pranavPic.png") {
+    } else if (url.pathname === "/components/NavBar.js") {
+      return new Response(await Deno.readFile("components/NavBar.js"), {
+        headers: { "content-type": "text/javascript" },
+      });
+    }
+
+    // images
+    else if (url.pathname === "/images/pranavPic.png") {
       return new Response(await Deno.readFile("images/pranavPic.png"), {
         headers: { "content-type": "image/png" },
       });
